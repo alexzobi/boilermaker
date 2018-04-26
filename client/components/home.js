c40932756e95573f6ee56219ef3aca7dfdcaf38f
Alex Sobiloff <asobiloff@gmail.com>
2018-04-26 12:58:09 -0400

    add homepage

---
+++ client/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+/**
+ * COMPONENT
+ */
+export const Home = () => {
+  return (
+    <div className="homeStyle">
+      <h1>main home</h1>
+    </div>
+  )
+}
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, Home, Navbar} from './components'
 import {me} from './store'
 
 /**
@@ -19,14 +19,16 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/home" component={Home} />
+        <Navbar />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {
           isLoggedIn &&
-            <Switch>
+          <Switch>
               {/* Routes placed here are only available after logging in */}
-              <Route path="/home" component={UserHome} />
-            </Switch>
+            <Route path="/userHome" component={UserHome} />
+          </Switch>
         }
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
